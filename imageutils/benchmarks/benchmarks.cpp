c5f159e14d02b7654225360df92afdbad7408a85
Rajmund Kail <raygerlabs@gmail.com>
2023-12-04 03:09:26 +0100

    Add benchmarking

---
+++ imageutils/benchmarks/benchmarks.cpp
@@ -0,0 +1,36 @@
+#include <benchmark/benchmark.h>
+
+
+#include "imageutils/image.hpp"
+#include "imageutils/image_filters.hpp"
+#include "imageutils/image_processor.hpp"
+
+using namespace imageutils;
+using namespace imageutils::filters;
+
+// Register function as benchmark
+static void
+BM_ImageProcessing(benchmark::State& state)
+{
+  constexpr const auto kSourcePath = "./resources/lena.jpg";
+  image input_image(kSourcePath); 
+  image_processor processor;
+  processor += make_filter(brightness, 2.0f);
+  processor += make_filter(contrast, 2.0f);
+  processor += make_filter(resize, 150, 150);
+  processor += make_filter(vertical_flip);
+  processor += make_filter(horizontal_flip);
+  processor += make_filter(invert_colors);
+  processor += make_filter(grayscale);
+
+  for (auto _ : state)
+  {
+    image output_image = processor.apply(input_image);
+    benchmark::DoNotOptimize(output_image);
+  }
+}
+
+BENCHMARK(BM_ImageProcessing);
+
+// Run benchmarks
+BENCHMARK_MAIN();
