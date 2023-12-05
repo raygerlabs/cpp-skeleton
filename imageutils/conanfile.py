9bc85a5abb6eacf02a1c7ff597f05a1931b29591
Rajmund, Kail <raygerlabs@gmail.com>
2023-12-05 03:19:30 +0100

    Fix packaging

--- imageutils/conanfile.py
+++ imageutils/conanfile.py
@@ -1,4 +1,4 @@
-from os.path import join
+import os
 
 from conan import ConanFile
 from conan.tools.cmake import CMakeDeps, cmake_layout
@@ -12,14 +12,9 @@ class MainRecipe(ConanFile):
     build_policy = "missing"
 
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-
     def export_sources(self):
         export_list = { 
-          "CMakePresets.json", 
+          "CMakePresets.json",
           "CMakeLists.txt",
           "cmake/*", 
           "src/*", 
@@ -55,23 +50,19 @@ class MainRecipe(ConanFile):
     def build(self):
         config_mode = "dev" if self.settings.build_type == "Debug" else "ci"
         preset_name = f"{config_mode}-{self.settings.compiler}"
-        self.run("cmake --version")
-        self.run("ninja --version")
         self.run(f"cmake --workflow --fresh --preset {preset_name}", cwd=self.source_folder)
 
 
     def package(self):
-        copy(self, pattern="*.*",      dst=join(self.package_folder, "include"), src=join(self.source_folder, "include"))
-        copy(self, pattern="*.lib",    dst=join(self.package_folder, "lib"),     src=self.build_folder, keep_path=False)
-        copy(self, pattern="*.a",      dst=join(self.package_folder, "lib"),     src=self.build_folder, keep_path=False)
-        copy(self, pattern="*.dylib*", dst=join(self.package_folder, "lib"),     src=self.build_folder, keep_path=False)
-        copy(self, pattern="*.so*",    dst=join(self.package_folder, "lib"),     src=self.build_folder, keep_path=False)
-        copy(self, pattern="*.dll",    dst=join(self.package_folder, "bin"),     src=self.build_folder, keep_path=False)
+        copy(self, "*.hpp",    os.path.join(self.source_folder, "include"), os.path.join(self.package_folder, "include"))
+        copy(self, "*.h",      os.path.join(self.source_folder, "include"), os.path.join(self.package_folder, "include"))
+        copy(self, "*.dll",    os.path.join(self.source_folder, "build"),   os.path.join(self.package_folder, "bin"), keep_path=False)
+        copy(self, "*.so*" ,   os.path.join(self.source_folder, "build"),   os.path.join(self.package_folder, "lib"), keep_path=False)
+        copy(self, "*.dylib*", os.path.join(self.source_folder, "build"),   os.path.join(self.package_folder, "lib"), keep_path=False)
+        copy(self, "*.lib",    os.path.join(self.source_folder, "build"),   os.path.join(self.package_folder, "lib"), keep_path=False)
+        copy(self, "*.a",      os.path.join(self.source_folder, "build"),   os.path.join(self.package_folder, "lib"), keep_path=False)
 
 
     def package_info(self):
         self.cpp_info.libs = [self.name]
-        self.cpp_info.set_property("cmake_file_name", self.name)
-        self.cpp_info.set_property("cmake_target_name", f"{self.name}::{self.name}")
-        self.cpp_info.names["cmake_find_package"] = self.name
-        self.cpp_info.names["cmake_find_package_multi"] = self.name
+
